21f4e199f877b66147da034947986c60344917f1
johnpaulgarcia <garcia.teneight@gmail.com>
2019-07-31 11:50:01 +0800

    d

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,9 @@ import {createMuiTheme,MuiThemeProvider} from '@material-ui/core/styles';
 import {Grid} from '@material-ui/core';
 import {Provider} from 'react-redux';
 import {configureStore} from './store';
+import {PersistGate} from 'redux-persist/integration/react';
+const {store,persistor} = configureStore();
+
 const theme = createMuiTheme({
 })
 
@@ -11,8 +14,10 @@ const App = () => {
 	return(
 		
 		<Grid container direction="column" alignItems="center" justify="center">
-		<Provider store={configureStore()}>
-			<Router />
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<Router />
+			</PersistGate>
 		</Provider>
 		</Grid>
 		
